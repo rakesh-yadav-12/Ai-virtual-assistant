d11bd8af23d65cf7f7d78fed17e0d8b289ff95ea
Rakesh Yadav <ry7955724@gmail.com>
2025-12-20 22:14:11 +0530

    Fix string quotes in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import UserContextProvider from './context/userContext.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import UserContextProvider from "./context/userContext.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserContextProvider>
         <App />
       </UserContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
