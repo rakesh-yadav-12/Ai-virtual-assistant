5f9aaa838ca2e75ad1a2e35865dfa42ad3036e0b
Rakesh Yadav <ry7955724@gmail.com>
2025-12-21 11:21:14 +0530

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -33,7 +33,7 @@ function SignIn() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/signin",
+        "https://ai-virtual-assistant-8b.onrender.com/api/auth/signin",
         {
           email: formData.email,
           password: formData.password
