8add419d64e795acc8ccc672c3af97dec8ab51e2
Rakesh Yadav <ry7955724@gmail.com>
2026-01-08 14:01:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: "https://ai-virtual-assistant-15bb.onrender.com",
+        target: "https://ai-virtual-assistant-20b.onrender.com",
         changeOrigin: true,
         secure: false,
       }
